93587cfc41b80813db87812006396f52a00a594d
Teresa Gámez <t.gamez@phytec.de>
2013-09-02 10:07:13 +0200

    PCM051: Add timings for MT41J256M8HX15E 512MB RAM.
    The MT41J256M8HX15E 2x256M8 RAMs where a little bit faster than
    the new 1x512M16. To have optimized RAM timings we support also
    the The MT41J256M8HX15E 2x256M8 again.
    
    Signed-off-by: Teresa Gámez <t.gamez@phytec.de>

--- arch/arm/boards/pcm051/lowlevel.c
+++ arch/arm/boards/pcm051/lowlevel.c
@@ -14,7 +14,7 @@
 #include <mach/am33xx-generic.h>
 #include <mach/wdt.h>
 
-static const struct am33xx_cmd_control MT41J256M16HA15EIT_1x512M16_cmd = {
+static const struct am33xx_cmd_control pcm051_cmd = {
 	.slave_ratio0 = 0x40,
 	.dll_lock_diff0 = 0x0,
 	.invert_clkout0 = 0x1,
@@ -26,6 +26,7 @@ static const struct am33xx_cmd_control MT41J256M16HA15EIT_1x512M16_cmd = {
 	.invert_clkout2 = 0x1,
 };
 
+/* 1x512MB */
 static const struct am33xx_emif_regs MT41J256M16HA15EIT_1x512M16_regs = {
 	.emif_read_latency	= 0x6,
 	.emif_tim1		= 0x0888A39B,
@@ -41,6 +42,24 @@ static const struct am33xx_ddr_data MT41J256M16HA15EIT_1x512M16_data = {
 	.wr_dqs_slave_ratio0	= 0x3B,
 	.fifo_we_slave_ratio0	= 0x96,
 	.wr_slave_ratio0	= 0x76,
+};
+
+/* 2x256MB */
+static const struct am33xx_emif_regs MT41J256M8HX15E_2x256M8_regs = {
+	.emif_read_latency	= 0x6,
+	.emif_tim1		= 0x0668A39B,
+	.emif_tim2		= 0x26337FDA,
+	.emif_tim3		= 0x501F830F,
+	.sdram_config		= 0x61C04832,
+	.zq_config		= 0x50074BE4,
+	.sdram_ref_ctrl		= 0x0000093B,
+};
+
+static const struct am33xx_ddr_data MT41J256M8HX15E_2x256M8_data = {
+	.rd_slave_ratio0	= 0x3B,
+	.wr_dqs_slave_ratio0	= 0x85,
+	.fifo_we_slave_ratio0	= 0x100,
+	.wr_slave_ratio0	= 0xC1,
 };
 
 /**
@@ -68,9 +87,14 @@ static int pcm051_board_init(void)
 
 	pll_init(MPUPLL_M_600, 25, DDRPLL_M_303);
 
-	am335x_sdram_init(0x18B, &MT41J256M16HA15EIT_1x512M16_cmd,
+	if (IS_ENABLED(CONFIG_512MB_MT41J128M16_1x512M16))
+		am335x_sdram_init(0x18B, &pcm051_cmd,
 			&MT41J256M16HA15EIT_1x512M16_regs,
 			&MT41J256M16HA15EIT_1x512M16_data);
+	else if (IS_ENABLED(CONFIG_512MB_MT41J256M8HX15E_2x256M8))
+		am335x_sdram_init(0x18B, &pcm051_cmd,
+			&MT41J256M8HX15E_2x256M8_regs,
+			&MT41J256M8HX15E_2x256M8_data);
 
 	am33xx_uart0_soft_reset();
 	am33xx_enable_uart0_pin_mux();
